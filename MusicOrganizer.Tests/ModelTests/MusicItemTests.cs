84315f1201c031dd2ad2a87fd8b257dc2c8a1d70
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:09:54 -0700

    add test for GetFormat

--- MusicOrganizer.Tests/ModelTests/MusicItemTests.cs
+++ MusicOrganizer.Tests/ModelTests/MusicItemTests.cs
@@ -9,7 +9,7 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void MusicItemConstructor_CreatesInstanceofMusicItem_MusicItem()
     {
-      MusicItem newMusicItem = new MusicItem("test");
+      MusicItem newMusicItem = new MusicItem("test", "test");
       Assert.AreEqual(newMusicItem.GetType(), typeof(MusicItem));
     }
 
@@ -17,11 +17,22 @@ namespace MusicOrganizer.Tests
     public void GetTitle_ReturnsTitle_String()
     {
       string title = "Octavarium";
-      MusicItem newMusicItem = new MusicItem(title);
+      MusicItem newMusicItem = new MusicItem(title, "test");
 
       string result = newMusicItem.Title;
 
       Assert.AreEqual(title, result);
+    }
+
+    [TestMethod]
+    public void GetFormat_ReturnsMusicFormat_String()
+    {
+      string format = "Vinyl";
+      MusicItem newMusicItem = new MusicItem("test", format);
+
+      string result = newMusicItem.Format;
+
+      Assert.AreEqual(format, result);
     }
   }
 }
