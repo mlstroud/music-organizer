2a0960e24158a7273fc721a0bb3db1b916b6b26a
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:26:31 -0700

    add unique ID to MusicItem

--- MusicOrganizer/Models/MusicItem.cs
+++ MusicOrganizer/Models/MusicItem.cs
@@ -7,9 +7,19 @@ namespace MusicOrganizer.Models
     public string Title { get; set; }
     public string Format { get; set; }
     public static List<MusicItem> _instances = new List<MusicItem>();
+    public int Id { get; }
 
     public MusicItem(string title, string format)
     {
+      if (_instances.Count == 0)
+      {
+        Id = 1;
+      }
+      else
+      {
+        Id++;
+      }
+
       Title = title;
       Format = format;
       _instances.Add(this);
