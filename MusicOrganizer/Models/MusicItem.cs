b444757cdef327262ff6edd1cac38f314966fb37
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:21:12 -0700

    update GetAllItems to return list of all MusicItems

--- MusicOrganizer/Models/MusicItem.cs
+++ MusicOrganizer/Models/MusicItem.cs
@@ -1,14 +1,28 @@
+using System.Collections.Generic;
+
 namespace MusicOrganizer.Models
 {
   public class MusicItem
   {
     public string Title { get; set; }
     public string Format { get; set; }
+    public static List<MusicItem> _instances = new List<MusicItem>();
 
     public MusicItem(string title, string format)
     {
       Title = title;
       Format = format;
+      _instances.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<MusicItem> GetAllItems()
+    {
+      return _instances;
     }
   }
 }
