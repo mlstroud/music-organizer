94f06ed07b15f4fc508db78fcd8201aed34279db
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:05:49 -0700

    update constructor to store title

--- MusicOrganizer/Models/MusicItem.cs
+++ MusicOrganizer/Models/MusicItem.cs
@@ -2,6 +2,11 @@ namespace MusicOrganizer.Models
 {
   public class MusicItem
   {
+    public string Title { get; set; }
 
+    public MusicItem(string title)
+    {
+      Title = title;
+    }
   }
 }
