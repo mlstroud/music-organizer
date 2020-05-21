534057c3ac4aa547e6c0281e0bd7c6a04ae060cf
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:05:17 -0700

    add test for GetTitle

--- MusicOrganizer.Tests/ModelTests/MusicItemTests.cs
+++ MusicOrganizer.Tests/ModelTests/MusicItemTests.cs
@@ -9,8 +9,19 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void MusicItemConstructor_CreatesInstanceofMusicItem_MusicItem()
     {
-      MusicItem newMusicItem = new MusicItem();
+      MusicItem newMusicItem = new MusicItem("test");
       Assert.AreEqual(newMusicItem.GetType(), typeof(MusicItem));
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      string title = "Octavarium";
+      MusicItem newMusicItem = new MusicItem(title);
+
+      string result = newMusicItem.Title;
+
+      Assert.AreEqual(title, result);
     }
   }
 }
