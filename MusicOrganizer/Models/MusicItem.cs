2ae2ab31471e4cd49bca1f8620884f41089c2e10
Matt Stroud <lixingke06@gmail.com>
2020-05-21 10:10:16 -0700

    update constructor to store format

--- MusicOrganizer/Models/MusicItem.cs
+++ MusicOrganizer/Models/MusicItem.cs
@@ -3,10 +3,12 @@ namespace MusicOrganizer.Models
   public class MusicItem
   {
     public string Title { get; set; }
+    public string Format { get; set; }
 
-    public MusicItem(string title)
+    public MusicItem(string title, string format)
     {
       Title = title;
+      Format = format;
     }
   }
 }
